7fcbd8b125cd5b6cf4c8345796bff6d50e6bb6b3
Stella Choi <stellachoi@0587359726.wireless.umich.net>
2018-04-12 16:43:52 -0400

    add codes for flatterning, sorting, and aggregating SBL data

---
+++ codes/aggregate_frame_sbl.py
@@ -0,0 +1,117 @@
+""" This script is a basic aggregation routine.  It serves as a framework that can be modified to
+aggregate the flattened classification records from any project in a variety of ways, though it is
+most useful for question and drawing tasks.  Transcriptions and survey tasks are better handled
+with specialized scripts provided elsewhere in this repository."""
+
+# depending on the functions and library members needed, additional modules may be required:
+import csv
+import sys
+import json
+
+csv.field_size_limit(sys.maxsize)
+
+# set up the file locations (user specific) - There needs to be a input file and the aggregated
+# out put file.  The input file is assumed to be sorted on the field we want to aggregate over.
+location = r'/Users/stellachoi/Documents/SDL/zooniverse/SBL/' \
+           r'sorted_flattened_south-bend-lead.csv'
+outlocation = r'/Users/stellachoi/Documents/SDL/zooniverse/SBL/' \
+           r'aggregate_south-bend-lead.csv'
+
+field_1_name = 'testresult_exist'
+field_2_name = 'testresult_list'
+field_3_name = "test_result_exist_yes_percent"
+# The next section can contain any functions we wish to use to analyse, modify or manipulate the
+# aggregated data.  Once all the data has been collected together for a particular selection criteria
+# (usually by subject), we can perform any analysis that depends only on the aggregated data for that
+# selection and is independent of data aggregated for any other selection.  For example we can find the
+# average of some input over the classifications for some subject image, but we can not at this point
+# compare that to the average of the same field for other subjects which have not yet been aggregated.
+some_limit = -1
+
+def process_aggregation(subj, cl_counter, aggregated_bin_1, aggregated_bin_2):
+    # process the aggregated data for a subject.  The input variables are the function parameters and
+    # the out_put can be any consistent function of those variables.  Typical processing should include
+    # verifying enough (some_limit) valid classifications were aggregated (even though all subjects may be 
+    # retired, there may be cases not all the classifications are valid. Other processing could include 
+    # clustering drawing points, calculating vote fractions, applying a Bayesian pipeline, or simply
+    # modifying the order or presentation format of the aggregate data.
+    if cl_counter > some_limit: 
+        out_put_1 = aggregated_bin_1
+        out_put_2 = aggregated_bin_2
+        out_put_3 = aggregated_bin_1[1] / cl_counter * 100
+
+        # Once the aggregated data is processed and the out_put variables defined we can
+        # set up and write the aggregated and processed data to a file.  The field names can be chosen to
+        # make the data as useful as possible. They must match the fieldnames in the section below - both
+        # in order and spelling.
+        new_row = {'subject_ids': subj,
+                   'classifications': cl_counter,
+                   field_1_name: out_put_1,
+                   field_3_name: out_put_3,
+                   field_2_name: out_put_2
+                   }
+        writer.writerow(new_row)
+        return True
+    else:
+        return False
+
+
+# set up the output file names for the aggregated and processed data, and write the header.  The
+# fieldnames and order must match the write block:
+with open(outlocation, 'w', newline='') as file:
+    fieldnames = ['subject_ids',
+                  'classifications',
+                  field_1_name,
+                  field_3_name,
+                  field_2_name]
+    writer = csv.DictWriter(file, fieldnames=fieldnames)
+    writer.writeheader()
+
+    # set up to read the flattened file
+    with open(location) as f:
+        r = csv.DictReader(f)
+
+        # initialize a starting point subject and empty bins for aggregation
+        subject = ''
+        i = 1
+        bin_1 = ['' for count in range(0, 2)]   # example for a list of known length  ( here 5,
+        #  such as a answer vector for a question with 5 multiple allowed answers)
+        bin_2 = 0   # Python data structures (dictionaries, sets, tuples or literals)
+
+        # Loop over the flattened classification records
+        for row in r:
+            # read a row and pullout the flattened data fields we need to aggregate, or pass through.
+            new_subject = row['subject_ids']
+            new_user = row['user_name']
+            field_1 = json.loads(row[field_1_name])  # complex fields must be loaded as json objects
+            field_2 = row[field_2_name]
+
+            # test for a change in the selector - in this case the selector is the subject
+            if new_subject != subject:
+                if i != 1:  # if not the first line, we have aggregated all the classifications for
+                    # this subject and we can analyse the aggregated fields and output the results.
+                    process_aggregation(subject, i, bin_1, bin_2)
+
+                # To star the next group, reset the selector, those things we need to pass through,
+                # and the bins for the next aggregation.
+                i = 1
+                subject = new_subject
+                users = {new_user}
+                # or image_size that is tied to the subject_ids.
+                bin_1 = field_1
+                bin_2 = field_2
+
+            else:
+                # The selector has not yet changed so we continue the aggregation:
+                # First test for multiple classifications by the same user on this subject, and 
+                # if we want to use a fixed number of classifications and a few subjects have 
+                # more than the retirement limit (here set at 15).
+                if users != users | {new_user} and i <= 15: 
+                    users |= {new_user}
+                    for count in range(0, len(field_1)):  # summing the elements of a answer_vector
+                        bin_1[count] += field_1[count]
+                    bin_2 += field_2  # typical aggregation for a field which can be summed
+                    i += 1
+
+        # catch and process the last aggregated group
+        process_aggregation(subject, i, bin_1, bin_2)
---
+++ codes/flatten_class_questions_sbl.py
@@ -0,0 +1,120 @@
+import csv
+import json
+import sys
+
+csv.field_size_limit(sys.maxsize)
+# file location area:
+location = r'/Users/stellachoi/Documents/SDL/zooniverse/SBL/' \
+           r'south-bend-lead-identification-project-classifications-csv.csv'
+out_location = r'/Users/stellachoi/Documents/SDL/zooniverse/SBL/flatten_classification_south-bend-lead.csv'
+#name_location = r'C:\py\FFIPusers\IPuser.csv'
+
+# define functions area:
+
+
+def include(class_record):
+    if int(class_record['workflow_id']) == 6071:
+        pass
+    else:
+        return False
+    if float(class_record['workflow_version']) >= 41.75:
+        pass
+    else:
+        return False
+    return True
+
+
+with open(out_location, 'w', newline='') as file:
+    # Note we have added a number of fields including 'line_number and changed the order to suit our whims -
+    # The write statement must match both items and order
+    fieldnames = ['line_number',
+                  'subject_ids',
+                  'user_name',
+                  'workflow_id',
+                  'workflow_version',
+                  'classification_id',
+                  'created_at',
+                  'testresult_exist',
+                  'testresult_list']
+
+    writer = csv.DictWriter(file, fieldnames=fieldnames)
+    writer.writeheader()
+
+    # Initialize and load pick lists
+    i = 0
+    j = 0
+    #name_list = load_pick_ip()
+    task_answer_template_1 = ['Yes', 'No']
+    with open(location) as f:
+        r = csv.DictReader(f)
+        for row in r:
+            i += 1
+            if i == 3000:
+                break
+            if include(row) is True:
+                j += 1
+                metadata = json.loads(row['metadata'])
+                annotations = json.loads(row['annotations'])
+
+                # Area to add the blocks that work on each record of the classification
+
+                # generate user_name for not_signed_in users
+                user_name = str(row['user_name'])
+                if row['user_id'] == '':
+                    user_name = 'Visitor'
+                    #  user_name = str(row[user_name])
+                    #  user_name = row['user_ip']
+
+                task_vector_1 = [0, 0]
+                task_answer_2 = ''
+                task_answer_3 = ''
+
+                for task in annotations:
+                    # Even though the first question only allows single answers we will use Block 4 - Multiple-
+                    # allowed answer question - answer vector method to record it:
+                    try:
+                        if 'T2' == task['task']:
+                            for k in range(0, 2):
+                                if task_answer_template_1[k] in task['value']:
+                                    task_vector_1[k] = 1
+                            if task_vector_1 == [0, 0]:  # no answers selected
+                                task_vector_1 = []
+                    except (TypeError, KeyError):
+                        task_vector_1 = ''
+                        continue
+                    # The second question is a single required answer with short answers that need no massaging
+                    try:
+                        if 'T1' == task['task']:
+                            if task['value'] is not None:
+                                task_answer_2 = str(task['value'])
+                    except KeyError:
+                        continue
+                    # The third question is also a simple single answer. These are short answers except for one
+                    # we will shorten:
+                    """
+                    try:
+                        if 'made of' in task['task_label']:
+                            if task['value'] is not None:
+                                task_answer_3 = str(task['value'])
+                                if task_answer_3.find('Silt') >= 0:
+                                    task_answer_3 = 'Mud'
+                    except KeyError:
+                        continue
+                    """
+                task_vector_1 = json.dumps(task_vector_1)
+
+                # Writer must agree with open field names and assign correct values to the fields
+                writer.writerow({'line_number': str(i),
+                                 'subject_ids': row['subject_ids'],
+                                 'user_name': user_name,
+                                 'workflow_id': row['workflow_id'],
+                                 'workflow_version': row['workflow_version'],
+                                 'classification_id': row['classification_id'],
+                                 'created_at': row['created_at'],
+                                 'testresult_exist': task_vector_1,
+                                 'testresult_list': task_answer_2})
+            print(i, j)
+        # Area to print final status report
+        print(i, 'lines read and inspected', j, 'records processed and copied')
+
+        #
